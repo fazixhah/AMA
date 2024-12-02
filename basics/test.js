8b8df9aaa7bd1949207c35bdd1e705317a5d165c
fazixhah <fazishah08@gmail.com>
2024-12-02 18:39:26 +0000

    new done

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,19 @@
-console.log("Faizan is great");
+// console.log("Faizan is great");
+// let myName = 33;
+// let stringMyName = String(myName)
+// console.log(myName);
+// console.log(typeof myName);
+// console.log(stringMyName);
+// console.log(typeof stringMyName);
+
+let newDate = "Faizan";
+let changedString = Number(newDate);
+
+console.log(newDate);
+console.log(typeof newDate);
+console.log(changedString);
+console.log(typeof changedString);
+
+
+
+
